eb9790ba533939f03d1397dbeeea062b25ed3ffb
Alexander <paulo.alexander12@gmail.com>
2020-04-11 17:49:47 -0500

    Final Capitulo 9 - Services Angular 9

--- Seccion9/services-start/src/app/new-account/new-account.component.ts
+++ Seccion9/services-start/src/app/new-account/new-account.component.ts
@@ -1,20 +1,30 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
   //A provider is an instruction to the Dependency Injection system on how to obtain a value for a dependency
-  providers: [LoggingService]
+  //providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
+  //@Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
 
-  constructor(private loggingService: LoggingService) {   
+  constructor(private loggingService: LoggingService,
+    private accountsService: AccountsService) {
+    this.accountsService.statusUpdated.subscribe(
+      (status: string) => alert('New Status: ' + status)
+    );
   }
 
   onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountsService.addAccount(accountName, accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
+  }
+
+  /*onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
@@ -23,5 +33,5 @@ export class NewAccountComponent {
     //console.log('A server status changed, new status: ' + accountStatus);
     // const service = new LoggingService();
     // service.logStatusChange(accountStatus);
-  }
+  }*/
 }
