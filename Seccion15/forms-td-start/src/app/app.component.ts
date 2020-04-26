3789ce51208fdbfc073acacb8bf0efc9033b2d7c
Alexander <paulo.alexander12@gmail.com>
2020-04-26 18:21:09 -0500

    clase 199 Solution  - Seccion 15 - Angular 9

--- Seccion15/forms-td-start/src/app/app.component.ts
+++ Seccion15/forms-td-start/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f', { static: true }) fomularioRegistro: NgForm;
+  
   defaultQuestion = 'pet';
   answer = '';
   genders = ['Male', 'Female'];
