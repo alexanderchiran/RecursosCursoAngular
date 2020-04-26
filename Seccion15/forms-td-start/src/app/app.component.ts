ad6c4262f8513f3f7660df0361b0c620e749ab15
Alexander <paulo.alexander12@gmail.com>
2020-04-26 18:02:24 -0500

    Clase 199 - Template-Driven forms - Angular 9

--- Seccion15/forms-td-start/src/app/app.component.ts
+++ Seccion15/forms-td-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,11 +7,55 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f', { static: true }) fomularioRegistro: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['Male', 'Female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion:'',
+    answer:'',
+    gender:''
+  };
+
+  submitted = false;
+
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    //primer enfoque de seteo de variables 
+    // this.fomularioRegistro.setValue(
+    //   {
+    //     userData1:{
+    //       username: suggestedName,
+    //       email:'algo@algo.com'
+    //     },
+    //     secret:'pet',
+    //     secret2:'pet',
+    //     questionAnswer:'123',
+    //     gender: 'Male'
+    //   }
+    // );
+
+    //segundo enfoque de seteo de variables
+    this.fomularioRegistro.form.patchValue({userData1: {username: suggestedName}})
   }
 
-  onSubmit1(form: NgForm) {
-    console.log(form);
+  // onSubmit1(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit1() {
+    this.submitted= true;
+    console.log(this.fomularioRegistro);
+    this.user.username = this.fomularioRegistro.value.userData1.username;
+    this.user.email = this.fomularioRegistro.value.userData1.email;
+    this.user.secretQuestion = this.fomularioRegistro.value.secret;
+    this.user.answer = this.fomularioRegistro.value.questionAnswer;
+    this.user.gender = this.fomularioRegistro.value.gender;
+
+    //RESET THE FORM    
+    this.fomularioRegistro.reset();
   }
 }
