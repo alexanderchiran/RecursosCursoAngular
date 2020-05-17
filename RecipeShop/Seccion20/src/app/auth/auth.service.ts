ef3a61f89e57c018a2687495658e5ebc3258c5d7
Alexander <paulo.alexander12@gmail.com>
2020-05-17 18:41:04 -0500

    Elimina Autenticación

--- RecipeShop/Seccion20/src/app/auth/auth.service.ts
+++ RecipeShop/Seccion20/src/app/auth/auth.service.ts
@@ -22,8 +22,8 @@ export class AuthService {
   //user = new Subject<User>();
   private tokenExpirationTimer: any;
   
-  signUpApiRestService='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAxsePdycyRQ3QMDgWPa0fKVnRlorvChLI'
-  loginApiRestService='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAxsePdycyRQ3QMDgWPa0fKVnRlorvChLI'
+  signUpApiRestService='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=XXX'
+  loginApiRestService='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=XXXX'
 
   constructor(private http: HttpClient, private router: Router) {}
 
